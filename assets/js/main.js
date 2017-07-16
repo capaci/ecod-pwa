2031ad3463c8a98a48376f1e925f4c0e4d2e6df8
capaci <rafaelcp123@gmail.com>
2017-07-16 10:13:31 -0300

    página para consulta do doutor

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,4 +26,17 @@ $(document).ready(function() {
 
     // $('ul.tabs').tabs({'select_tab': 'tab_id', 'swipeable': true});
 
+    $('.timepicker').pickatime({
+        default: 'now', // Set default time
+        fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
+        twelvehour: false, // Use AM/PM or 24-hour format
+        donetext: 'OK', // text for done-button
+        cleartext: 'Clear', // text for clear-button
+        canceltext: 'Cancel', // Text for cancel-button
+        autoclose: false, // automatic close timepicker
+        ampmclickable: true, // make AM PM clickable
+        aftershow: function(){} //Function for after opening timepicker
+    });
+    
+
 });
